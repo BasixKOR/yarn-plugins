6b4e23f121a1c74f775a8d83ac1867ff5c92b684
Basix <basix@basix.tech>
2024-07-18 18:30:57 +0900

    Initialize yarn-plugin-sensible

---
+++ packages/sensible/sources/index.ts
@@ -0,0 +1,30 @@
+import {Plugin, Hooks} from '@yarnpkg/core';
+import {BaseCommand} from '@yarnpkg/cli';
+import {Option} from 'clipanion';
+
+class HelloWorldCommand extends BaseCommand {
+  static paths = [
+    [`hello`, `world`],
+  ];
+
+  name = Option.String(`--name`, `John Doe`, {
+    description: `Your name`,
+  });
+
+  async execute() {
+    console.log(`Hello ${this.name}!`);
+  }
+}
+
+const plugin: Plugin<Hooks> = {
+  hooks: {
+    afterAllInstalled: () => {
+      console.log(`What a great install, am I right?`);
+    },
+  },
+  commands: [
+    HelloWorldCommand,
+  ],
+};
+
+export default plugin;
